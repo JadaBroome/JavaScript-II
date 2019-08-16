17619661b19dc991e12e1f2cac8dd320b6a22757
JadaBroome <49737623+JadaBroome@users.noreply.github.com>
2019-08-15 19:12:44 -0700

    finsihed Javascript II

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,28 +56,57 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+const mapFullName = runners.map((arr) => {
+    return {'First Name': arr.first_name, 'Last Name': arr.last_name};
+});
+
+fullName.push(mapFullName);
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
+const mapAllCaps = runners.map((arr) => {
+    return{'First Name': arr.first_name.toUpperCase()}
+});
+allCaps.push(mapAllCaps);
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
+const filterLargeShirts = runners.filter((arr) => {
+    return arr.shirt_size <= "L";
+});
+largeShirts.push(filterLargeShirts);
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
+const reduceTicketPriceTotal = runners.reduce((total, arr) => {
+    return total += arr.donation;
+}, 0);
+ticketPriceTotal.push(reduceTicketPriceTotal);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+let companyName =[];
+const filterCompanyName = runners.filter((arr) => {
+    return arr.company_name === 'Skinix' || arr.company_name ==='Skinte';
+})
+companyName.push(filterCompanyName);
+console.log(companyName);
 
 // Problem 2
-
+let lastNames= [];
+const sortLastNames = runners.map((arr) => {
+    return arr.last_name 
+});
+sortLastNames.sort();
+lastNames.push(sortLastNames);
+console.log(lastNames);
 // Problem 3
--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,44 +2,61 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
+
 
   //Given this problem: 
   
-  function firstItem(arr, cb) {
-    // firstItem passes the first item of the given array to the callback function.
-  }
+  // function firstItem(arr, cb) {
+  //   // firstItem passes the first item of the given array to the callback function.
+  // }
 
   // Potential Solution:
 
   // Higher order function using "cb" as the call back
-  function firstItem(arr, cb) {
-    return cb(arr[0]);
-  }
+  // function firstItem(arr, cb) {
+  //   return cb(arr[0]);
+  // }
 
-  // Function invocation 
-  firstItem(items, function(first) {
-    console.log(first)
-  });
+  // // Function invocation 
+  // firstItem(items, function(first) {
+  //   console.log(first)
+  // });
 
-*/
+
 
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, function(length) {
+  console.log(length);
+}); 
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  // let lastItem = arr[arr.length - 1];
+  // cb(lastItem);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y); 
 }
+sumNums(4, 3, function(add) {
+  console.log(add)
+})
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb (x * y);
 }
+
+multiplyNums(10, 5, function(multiply) {
+  console.log(multiply);
+})
+
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
@@ -53,3 +70,10 @@ function removeDuplicates(array, cb) {
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
 }
+
+const lastName = 'Bob';
+function greet() { 
+  const firstName = 'Jim';
+  alert(`The name's ${lastName}, ${firstName} ${lastName}`);
+}
+greet();
